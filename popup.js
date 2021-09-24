366563b02244dd71965b08bb5a06489c521fe4eb
zed.cole <50928706+ZedCole@users.noreply.github.com>
2021-09-24 11:18:53 +1000

    Added countdown functionality and test draw area

--- popup.js
+++ popup.js
@@ -1,26 +1,44 @@
-function dToday(){
-    var d = new Date();
-    var second = d.getUTCSeconds();
-    var minute = d.getUTCMinutes();
-    var offset = d.getTimezoneOffset() / 60;
-    var hour = d.getUTCHours() - offset;
-    var day = d.getUTCDate();
-    var month = d.getUTCMonth();
-    var year = d.getUTCFullYear();
+var dateHalloween = new Date(); 
+var dateToday = new Date();
+var writeArea = document.getElementById('countdown');
+var c_days,c_hours,c_minutes,c_seconds
+const _DATE_FORMATING = 1000 * 60 * 60 * 24;
 
-    var now = day + '/' + month +'/' + year + ' ' + hour + ':' + minute + ':' + second;
-
-    console.log(now);
-
-    hall = new Date(year, 9, 31);
-    console.count(hall);
+function getDateToday(){
+    var today = new Date();
+    var offset = today.getTimezoneOffset() / 60;
+    today.setUTCHours(today.getUTCHours() - offset);
+    dateToday = today;
 }
 
-dToday();
+function getDateHalloween() {
+    var today = new Date();
+    var offset = today.getTimezoneOffset() / 60;
+    today.setUTCHours(today.getUTCHours() - offset);
+    if (today.getUTCMonth() > 9) {
+        y = today.getUTCFullYear() + 1;
+        dateHalloween = new Date(Date.UTC(y,9,31, offset));
+    } else {
+        y = today.getUTCFullYear();
+        dateHalloween = new Date(Date.UTC(y,9,31, offset));
+    }
+}
 
-// Check if Halloween has already been this year
-function whenIsHalloween() {
+function timeDifference(endDate, startDate) {
+    difference = endDate - startDate;
+    c_days = Math.floor(difference / _DATE_FORMATING);
+    c_hours = Math.floor((difference % _DATE_FORMATING) / (1000 * 60 * 60));
+    c_minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    c_seconds = Math.floor((difference % (1000 * 60)) / 1000);
+}
+
+function drawCountdown() {
+    getDateToday();
+    getDateHalloween();
+    timeDifference(dateHalloween, dateToday);
+    writeArea.innerHTML = c_days + " days " + c_hours + " hours " + c_minutes + " minutes " + c_seconds + " seconds Until Halloween!";
 }
 
 // Uncomment when draw function is ready
-//setInterval(draw, 500);
+drawCountdown();
+setInterval(drawCountdown, 500);
