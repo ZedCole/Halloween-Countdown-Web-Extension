bf31135ddf832fc4bfa82cbf880316a11df73bc7
zed.cole <50928706+ZedCole@users.noreply.github.com>
2021-09-25 20:57:10 +1000

    Extension now displays a countdown

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A small chrome extension to countdown to Halloween. Will be Halloween themed dur
 ## To Do
  - [x] Create canvas in popup to draw countdown to
  - [x] Find a spooky font
- - [ ] Draw Countdown to Canvas
+ - [x] Draw Countdown to Canvas
  - [ ] Update icon
  - [ ] Change popups theme during October
     - [ ] Create some animations
--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,13 @@
 var dateHalloween = new Date(); 
 var dateToday = new Date();
 var writeArea = document.getElementById('countdown');
+var ctx = writeArea.getContext('2d');
 var isItOctober = false;
 var isTodayHalloween = false;
 var c_days,c_hours,c_minutes,c_seconds
 const _DATE_FORMATING = 1000 * 60 * 60 * 24;
+
+ctx.textAlign = 'center';
 
 function getDateToday(){
     var today = new Date();
@@ -14,7 +17,7 @@ function getDateToday(){
 }
 
 function getDateHalloween() {
-    var today = new Date(2021,9,30);
+    var today = new Date();
     var offset = today.getTimezoneOffset() / 60;
     today.setUTCHours(today.getUTCHours() - offset);
     if (today.getUTCMonth() > 9) { // Past October
@@ -38,15 +41,87 @@ function timeDifference(endDate, startDate) {
     c_hours = Math.floor((difference % _DATE_FORMATING) / (1000 * 60 * 60));
     c_minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     c_seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    time_array = [c_days,c_hours,c_minutes,c_seconds];
+    if (c_days < 10) {
+        c_days.toString();
+        c_days = "0" + c_days;
+    }
+    if (c_hours < 10) {
+        c_hours.toString();
+        c_hours = "0" + c_hours;
+    }
+    if (c_minutes < 10) {
+        c_minutes.toString();
+        c_minutes = "0" + c_minutes;
+    }
+    if (c_seconds < 10) {
+        c_seconds.toString();
+        c_seconds = "0" + c_seconds;
+    }
+}
+
+function fontLoad() {
+    ctx.font ='20px Creepster-Regular';
+    rem = document.getElementById("remove");
+    rem.remove();
+}
+
+function clearCanvas() {
+    w = writeArea.width;
+    h = writeArea.height;
+    ctx.fillStyle = '#ff9900';
+    ctx.fillRect(0,0,w,h)
 }
 
 function drawCountdown() {
+    w = writeArea.width / 8;
+    h = writeArea.height / 4;
+    ctx.fillStyle = '#000000';
+    ctx.font ='40px Creepster-Regular';
+    ctx.fillText(c_days,(w),(h*2));
+    ctx.fillText(":",(w*2),(h*2));
+    ctx.fillText(c_hours,(w*3),(h*2));
+    ctx.fillText(":",(w*4),(h*2));
+    ctx.fillText(c_minutes,(w*5),(h*2));
+    ctx.fillText(":",(w*6),(h*2));
+    ctx.fillText(c_seconds,(w*7),(h*2));
+    ctx.font ='15px Creepster-Regular';
+    ctx.fillText("DAYS",(w),((h*2)+20));
+    ctx.fillText("HOURS",(w*3),((h*2)+20));
+    ctx.fillText("MINUTES",(w*5),((h*2)+20));
+    ctx.fillText("SECONDS",(w*7),((h*2)+20));
+}
+
+function normText() {
+    w = writeArea.width / 2;
+    ctx.fillStyle = '#000000';
+    ctx.font ='30px Creepster-Regular';
+    ctx.fillText("HALLOWEEN COUNTDOWN",w,30);
+}
+
+function octText() {
+    w = writeArea.width / 2;
+    ctx.fillStyle = '#000000';
+    ctx.font ='30px Creepster-Regular';
+    ctx.fillText("IT'S SPOOKY SEASON",w,30);
+}
+
+function mainLoop() {
     getDateToday();
     getDateHalloween();
     timeDifference(dateHalloween, dateToday);
-    writeArea.innerHTML = c_days + " days " + c_hours + " hours " + c_minutes + " minutes " + c_seconds + " seconds Until Halloween!";
+    clearCanvas();
+    if (isTodayHalloween == true) {
+        
+    } else {
+        if (isItOctober == true) {
+            octText();
+        } else {
+            normText();
+        }
+        drawCountdown();
+    }
 }
 
-// Uncomment when draw function is ready
-drawCountdown();
-setInterval(drawCountdown, 500);
+fontLoad();
+setInterval(mainLoop, 100);
